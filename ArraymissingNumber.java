162e92e3062aa024076aa8ffd32c6d203cabf9d7
krushnashende80 <krushnashende80@gmail.com>
2026-03-24 00:17:18 +0530

    Create ArraymissingNumber.java
    Find the missing element in array so use here searching technique  using for loop index.
    
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

---
+++ ArraymissingNumber.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        Arrays.sort(nums);   // sort the array
+        // check from 0 to n
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;   // missing number found
+            }
+        }
+        
+        // if all elements are correct, missing is n
+        return nums.length;
+    }
+}
