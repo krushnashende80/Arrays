734e52364b9338317b6398585f74a222da982420
krushnashende80 <krushnashende80@gmail.com>
2026-03-25 00:00:58 +0530

    Update WaveArray.java
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

--- WaveArray.java
+++ WaveArray.java
@@ -1,12 +1,11 @@
 class Solution {
     public void sortInWave(int arr[]) {
         // code here
-         for(int i=0;i<arr.length-1;i =i++){
-             if(i+1%2==0){
-             int temp = arr[i];
-             arr[i] = arr[i-1];
-             arr[i-1] = temp;
+         for(int i=0;i<arr.length-1;i=i+2){
+            
+             int temp = arr[i+1];
+             arr[i+1] = arr[i];
+             arr[i] = temp;
         }
      }
-}
 }
