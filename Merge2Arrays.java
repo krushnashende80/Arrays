b73c976ea26090c4d75b43dc2c8ffa90379ef08c
krushnashende80 <krushnashende80@gmail.com>
2026-03-28 01:39:12 +0530

    Create Merge2Arrays.java
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

---
+++ Merge2Arrays.java
@@ -0,0 +1,24 @@
+public class Merge2Arrays {
+    public static void main(String[] args) {
+
+        int[] fst = { 1, 2, 3, 4, 5 };
+        int[] sec = { 6, 7, 8, 9, 10 };
+        int[] third = new int[fst.length + sec.length];
+
+        for (int i = 0; i < fst.length; i++) {
+            third[i] = fst[i];
+        }
+        int n = 0;
+        int k = third.length / 2;
+        int n1 = sec.length;
+        while (n < n1) {
+            third[k] = sec[n];
+            n++;
+            k++;
+        }
+
+        for (int print : third) {
+            System.out.print(print + " ");
+        }
+    }
+}
