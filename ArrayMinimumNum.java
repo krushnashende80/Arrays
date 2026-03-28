eb42d882d803cdf8abda67b11efe0b4e90a0d0f7
krushnashende80 <krushnashende80@gmail.com>
2026-03-28 23:51:24 +0530

    Create ArrayMinimumNum.java
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

---
+++ ArrayMinimumNum.java
@@ -0,0 +1,15 @@
+public class ArrayMinimumNum {
+    public static void main(String[] args) {
+
+        int[] arr = { 40, 39, 10, 330, 90 };
+        int temp = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < temp) {
+                temp = arr[i]; // max
+            }
+            // System.out.println(temp);
+        }
+        System.out.println(temp);
+    }
+}
