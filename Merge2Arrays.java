42c71b48d59aa33cd5a4c2c9c694fff36a21ebdc
krushnashende80 <krushnashende80@gmail.com>
2026-03-29 21:43:13 +0530

    Update Merge2Arrays.java
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

--- Merge2Arrays.java
+++ Merge2Arrays.java
@@ -6,7 +6,7 @@ public class Merge2Arrays {
         int[] third = new int[fst.length + sec.length];    
 
         for (int i = 0; i < fst.length; i++) {     
-            third[i] = fst[i];
+            third[i] = fst[i];   
         }
         int n = 0;
         int k = third.length / 2;
