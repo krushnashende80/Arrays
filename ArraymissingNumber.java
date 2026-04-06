988f620cba7aaba9a729ba1d18a76ff2579b9ca1
krushnashende80 <krushnashende80@gmail.com>
2026-04-06 23:45:22 +0530

    Update ArraymissingNumber.java
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

--- ArraymissingNumber.java
+++ ArraymissingNumber.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int missingNumber(int[] nums) {   
-        
+           
         Arrays.sort(nums);   // sort the array
         // check from 0 to n
         for (int i = 0; i < nums.length; i++) {
