d8ad4ebea61b88a834d9e8659a8b542567d6f372
krushnashende80 <krushnashende80@gmail.com>
2026-04-08 22:14:12 +0530

    Update bubbleSort.java
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,7 +1,7 @@
 class Solution {
     public void bubbleSort(int[] arr) {
         // code here  
-        
+           
         for(int i=0;i<arr.length;i++){ 
             for(int j=0;j<arr.length-1;j++){
                 if(arr[j]>arr[j+1]){
