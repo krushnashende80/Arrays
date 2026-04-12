bc1a6cca7666b9cfae1c251c90f3e6c7c17757e8
krushnashende80 <krushnashende80@gmail.com>
2026-04-12 18:26:28 +0530

    give an integer array so here we use bubble sort technique to sort array and return Kth number index value.
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

--- kthSmallestNum.java
+++ kthSmallestNum.java
@@ -1,7 +1,7 @@
 class kthSmallestNum {
     public int kthSmallest(int[] arr, int k) {
         // Code here
-        for(int i=0;i<arr.length-1;i++){
+        for(int i=0;i<arr.length-1;i++){  
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
