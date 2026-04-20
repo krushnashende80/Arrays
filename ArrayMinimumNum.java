9e678c70e15d7f8281175c6b8f4c0315f5ddcc3e
krushnashende80 <krushnashende80@gmail.com>
2026-04-20 22:56:01 +0530

    Update ArrayMinimumNum.java
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

--- ArrayMinimumNum.java
+++ ArrayMinimumNum.java
@@ -2,7 +2,7 @@ public class ArrayMinimumNum {
     public static void main(String[] args) {
 
         int[] arr = { 40, 39, 10, 330, 90 };
-        int temp = arr[0];
+        int temp = arr[0];    
 
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < temp) {
