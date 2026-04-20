539a5d1e096a1d291c7c00b869091ed3c07eaa66
krushnashende80 <krushnashende80@gmail.com>
2026-04-20 22:56:29 +0530

    Fix formatting issue in ArrayMinimumNum.java
    Signed-off-by: krushnashende80 <krushnashende80@gmail.com>

--- ArrayMinimumNum.java
+++ ArrayMinimumNum.java
@@ -1,7 +1,7 @@
 public class ArrayMinimumNum {
     public static void main(String[] args) {
 
-        int[] arr = { 40, 39, 10, 330, 90 };
+        int[] arr = { 40, 39, 10, 330, 90 };    
         int temp = arr[0];        
 
         for (int i = 1; i < arr.length; i++) {    
